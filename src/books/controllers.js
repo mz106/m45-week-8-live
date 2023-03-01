b9b6de2ce6cdb899b0c1a40207e5b079f4a74242
Michael Z <michaelzwierzanski@gmail.com>
2023-03-01 12:09:01 +0000

    add .create addBook

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,5 +1,36 @@
+const Book = require("./model");
+
 const addBook = async (req, res) => {
-  res.status(201).json({ message: "hello !" });
+  try {
+    // const book = await Book.create({
+    //   title: req.body.title,
+    //   author: req.body.author,
+    //   genre: req.body.genre,
+    // });
+
+    const book = await Book.create(req.body);
+
+    console.log(book);
+
+    // const successResponse = {
+    //   message: "success",
+    //   newBook: book,
+    // };
+
+    // res.status(201).json(successResponse);
+
+    res.status(201).json({ message: "success", newBook: book });
+  } catch (error) {
+    console.log(error);
+    // const errorResponse = {
+    //   errorMessage: error.message,
+    //   error: error,
+    // };
+
+    // res.status(501).json(errorResponse);
+
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
 };
 
 module.exports = {
