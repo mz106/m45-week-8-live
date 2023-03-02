77b0b0c3dd2cc596c3b1d277baf3079220bdeee5
Michael Z <michaelzwierzanski@gmail.com>
2023-03-02 11:18:26 +0000

    add stuff

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -33,6 +33,83 @@ const addBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.findAll();
+
+    res.status(200).json({ message: "success", books: allBooks });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.update(
+      { [req.body.updateKey]: req.body.updateValue },
+      { where: { title: req.body.title } }
+    );
+
+    res.status(204).json({ message: "success", updatedBook });
+  } catch (error) {
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const result = await Book.destroy({ where: { title: req.body.title } });
+
+    res.status(201).json({ message: "success", result: result });
+  } catch (error) {
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
+};
+
+const getSingleBookByTitle = async (req, res) => {
+  try {
+    const book = await Book.findOne({ where: { title: req.params.title } });
+    res.status(200).json({ message: "success", book: book });
+  } catch (error) {
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
+};
+
+// await User.destroy({
+//   where: {
+//     firstName: "Jane",
+//   },
+// });
+
 module.exports = {
   addBook,
+  getAllBooks,
+  updateBook,
+  deleteBook,
+  getSingleBookByTitle,
 };
+
+//mongoose
+
+//update the book "matilda" genre from childrens to kids
+// const updatedBook = await Book.updateOne(
+//   { title: "matilda" }, //filter
+//   { genre: "kids" } //new data to update to
+// );
+
+// {
+//   "title": "matilda",
+//   "author": "roald dahl",
+//   "genre": "childrens"
+// }
+
+// //sequelize
+// const updateBook = await Book.update(
+//   { genre: "kids" }, // new data to update to
+//   { where: { title: "matilda" } } // filter
+// );
+
+// state bookTitle
+
+// const response = await fetch(`http://localhost:5001/books/getbook/${bookTitle}`)
--- src/books/routes.js
+++ src/books/routes.js
@@ -2,8 +2,22 @@ const { Router } = require("express");
 
 const bookRouter = Router();
 
-const { addBook } = require("./controllers");
+const {
+  addBook,
+  getAllBooks,
+  updateBook,
+  deleteBook,
+  getSingleBookByTitle,
+} = require("./controllers");
 
 bookRouter.post("/books/addbook", addBook);
+bookRouter.get("/books/getallbooks", getAllBooks);
+
+//http://localhost:5001/books/getbook/beans on toast
+// {dave: beans on toast}
+
+bookRouter.get("/books/getbook/:title", getSingleBookByTitle);
+bookRouter.put("/books/updatebook", updateBook);
+bookRouter.delete("/books/deletebook", deleteBook);
 
 module.exports = bookRouter;
